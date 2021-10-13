f52d0bee4ff87585684127a37f821fd7533408b9
Nathan May <nathaniel.may22@gmail.com>
2021-10-13 14:30:31 -0400

    first commit

---
+++ js/problem_4.js
@@ -0,0 +1,6 @@
+const messageInput = document.querySelector('#problem-4 #message-input');
+const messageFeedback = document.querySelector('#problem-4 #message-feedback')
+
+const MAX_CHARACTERS = 50;
+
+// Write your code here
